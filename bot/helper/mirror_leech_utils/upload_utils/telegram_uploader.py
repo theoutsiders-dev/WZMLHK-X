705fd886ffe0c24f9087cdd2e942334c1acf83a8
PainKiller3 <ninadpatil100@gmail.com>
2026-09-26 13:50:55 +0530

    fix(leech): route private uploads to user PM or dump chat exclusively when PRIVATE_OUTPUT is set

--- bot/helper/mirror_leech_utils/upload_utils/telegram_uploader.py
+++ bot/helper/mirror_leech_utils/upload_utils/telegram_uploader.py
@@ -201,31 +201,33 @@ class TelegramUploader:
                 return False
 
         elif (self._bot_pm or self._private_output) and self._listener.is_super_chat:
-            if Config.LEECH_DUMP_CHAT:
-                try:
-                    msg_link = self._listener.message.link
-                    msg = f"""➲ <b><u>Leech Started :</u></b>
+            dest_id = (
+                int(Config.LEECH_DUMP_CHAT)
+                if Config.LEECH_DUMP_CHAT
+                else self._listener.user_id
+            )
+            try:
+                msg_link = self._listener.message.link
+                msg = f"""➲ <b><u>Leech Started :</u></b>
 ┃
 ┠ <b>User :</b> {self._listener.user.mention} ( #ID{self._listener.user_id} )\n┠ <b>Message Link :</b> <a href='{msg_link}'>Click Here</a>
 ┖ <b>Source :</b> <a href='{self._listener.source_url}'>Click Here</a>"""
-                    self._log_msg = await TgClient.bot.send_message(
-                        chat_id=int(Config.LEECH_DUMP_CHAT),
-                        text=msg,
-                        disable_web_page_preview=True,
-                        disable_notification=True,
+                self._log_msg = await TgClient.bot.send_message(
+                    chat_id=dest_id,
+                    text=msg,
+                    disable_web_page_preview=True,
+                    disable_notification=True,
+                )
+                self._sent_msg = self._log_msg
+                if self._user_session:
+                    self._sent_msg = await TgClient.user.get_messages(
+                        chat_id=self._sent_msg.chat.id,
+                        message_ids=self._sent_msg.id,
                     )
-                    self._sent_msg = self._log_msg
-                    if self._user_session:
-                        self._sent_msg = await TgClient.user.get_messages(
-                            chat_id=self._sent_msg.chat.id,
-                            message_ids=self._sent_msg.id,
-                        )
-                except Exception as e:
-                    LOGGER.error(f"Failed to send leech start to LEECH_DUMP_CHAT: {e}")
-                    self._sent_msg = self._listener.pm_msg or self._listener.message
-            elif self._listener.pm_msg:
-                self._sent_msg = self._listener.pm_msg
-            else:
+            except Exception as e:
+                LOGGER.error(
+                    f"Failed to send leech start to private destination ({dest_id}): {e}"
+                )
                 self._sent_msg = self._listener.message
         elif self._user_session:
             self._sent_msg = await TgClient.user.get_messages(
